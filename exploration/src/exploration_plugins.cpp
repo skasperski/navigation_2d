8ef32b40367c550a2836b7673865ddfeb0da7ccd
Sebastian Kasperski <sebastian.kasperski@dfki.de>
2014-02-18 13:47:56 +0100

    Cleared workspace for catkin creation.

--- exploration/src/exploration_plugins.cpp
+++
@@ -1,9 +0,0 @@
-#include <pluginlib/class_list_macros.h>
-#include <ExplorationPlanner.h>
-#include <exploration/NearestFrontierPlanner.h>
-#include <exploration/MultiWavefrontPlanner.h>
-#include <exploration/MinPosPlanner.h>
-
-PLUGINLIB_DECLARE_CLASS(exploration, NearestFrontier, NearestFrontierPlanner, ExplorationPlanner)
-PLUGINLIB_DECLARE_CLASS(exploration, MultiWavefront, MultiWavefrontPlanner, ExplorationPlanner)
-PLUGINLIB_DECLARE_CLASS(exploration, MinPos, MinPosPlanner, ExplorationPlanner)
