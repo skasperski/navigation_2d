1bedb4c32ec52d08a39d19a5cab13b83cc7b177d
Sebastian Kasperski <sebastian.kasperski@dfki.de>
2014-05-12 14:48:57 +0200

    Translated graph structure from karto to g2o.

--- nav2d_karto/src/G2oSolver.cpp
+++ nav2d_karto/src/G2oSolver.cpp
@@ -4,12 +4,11 @@
 #include "g2o/core/factory.h"
 #include "g2o/core/optimization_algorithm_factory.h"
 #include "g2o/core/optimization_algorithm_gauss_newton.h"
-
 #include "g2o/solvers/csparse/linear_solver_csparse.h"
 #include "g2o/solvers/cholmod/linear_solver_cholmod.h"
+#include "g2o/types/slam2d/types_slam2d.h"
 
-#include "g2o/types/slam2d/vertex_se2.h"
-#include "g2o/types/slam2d/edge_se2.h"
+#include <ros/console.h>
 
 typedef g2o::BlockSolver< g2o::BlockSolverTraits<-1, -1> > SlamBlockSolver;
 typedef g2o::LinearSolverCSparse<SlamBlockSolver::PoseMatrixType> SlamCSparseLinearSolver;
@@ -45,15 +44,45 @@ void G2oSolver::Compute()
 
 void G2oSolver::AddNode(karto::Vertex<karto::LocalizedObjectPtr>* pVertex)
 {
-	karto::Pose2 odom = pVertex->GetVertexObject()->GetOdometricPose(); 
+	karto::Pose2 odom = pVertex->GetVertexObject()->GetCorrectedPose(); 
 	g2o::VertexSE2* poseVertex = new g2o::VertexSE2;
 	poseVertex->setEstimate(g2o::SE2(odom.GetX(), odom.GetY(), odom.GetHeading()));
+	poseVertex->setId(pVertex->GetVertexObject()->GetUniqueId());
 	mOptimizer.addVertex(poseVertex);
 }
 
 void G2oSolver::AddConstraint(karto::Edge<karto::LocalizedObjectPtr>* pEdge)
 {
+	// Create a new edge
+	g2o::EdgeSE2* odometry = new g2o::EdgeSE2;
 	
+	// Set source and target
+	int sourceID = pEdge->GetSource()->GetVertexObject()->GetUniqueId();
+	int targetID = pEdge->GetTarget()->GetVertexObject()->GetUniqueId();
+	odometry->vertices()[0] = mOptimizer.vertex(sourceID);
+	odometry->vertices()[1] = mOptimizer.vertex(targetID);
+	if(odometry->vertices()[0] == NULL) ROS_ERROR("Source vertex with id %d does not exist!", sourceID);
+	if(odometry->vertices()[0] == NULL) ROS_ERROR("Target vertex with id %d does not exist!", targetID);
+	
+	// Set the measurement (odometry distance between vertices)
+	karto::LinkInfo* pLinkInfo = (karto::LinkInfo*)(pEdge->GetLabel());
+	karto::Pose2 diff = pLinkInfo->GetPoseDifference();
+	g2o::SE2 measurement(diff.GetX(), diff.GetY(), diff.GetHeading());
+	odometry->setMeasurement(measurement);
+	
+	// Set the covariance of the measurement
+	karto::Matrix3 precisionMatrix = pLinkInfo->GetCovariance().Inverse();
+	Eigen::Matrix<double,3,3> info;
+	info(0,0) = precisionMatrix(0,0);
+	info(0,1) = info(1,0) = precisionMatrix(0,1);
+	info(0,2) = info(2,0) = precisionMatrix(0,2);
+	info(1,1) = precisionMatrix(1,1);
+	info(1,2) = info(2,1) = precisionMatrix(1,2);
+	info(2,2) = precisionMatrix(2,2);
+	odometry->setInformation(info);
+	
+	// Add the constraint to the optimizer
+	mOptimizer.addEdge(odometry);
 }
 
 const karto::ScanSolver::IdPoseVector& G2oSolver::GetCorrections() const
