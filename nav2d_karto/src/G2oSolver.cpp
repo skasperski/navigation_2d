94e99bbfd6a96e64476ed3622b1468a96943e8c8
Sebastian Kasperski <sebastian.kasperski@dfki.de>
2014-05-13 11:42:49 +0200

    g2o backend mostly working.

--- nav2d_karto/src/G2oSolver.cpp
+++ nav2d_karto/src/G2oSolver.cpp
@@ -34,12 +34,47 @@ G2oSolver::~G2oSolver()
 void G2oSolver::Clear()
 {
 	// freeing the graph memory
-	mOptimizer.clear();
+	ROS_INFO("[g2o] Clearing Optimizer..."); 
+	mCorrections.Clear();
 }
 
 void G2oSolver::Compute()
 {
+	// Fix the first node in the graph to hold the map in place
+	g2o::OptimizableGraph::Vertex* first = mOptimizer.vertex(0);
+	if(!first)
+	{
+		ROS_ERROR("[g2o] No Node with ID 0 found!");
+		return;
+	}
+	first->fixed();
 	
+	// Do the graph optimization
+	mOptimizer.initializeOptimization();
+	int iter = mOptimizer.optimize(500);
+	if (iter > 0)
+	{
+		ROS_INFO("[g2o] Optimization finished after %d iterations.", iter);
+	}else
+	{
+		ROS_ERROR("[g2o] Optimization failed, result might be invalid!");
+		return;
+	}
+	
+	// Write the result so it can be used by the mapper
+	g2o::SparseOptimizer::VertexContainer nodes = mOptimizer.activeVertices();
+	for (g2o::SparseOptimizer::VertexContainer::const_iterator n = nodes.begin(); n != nodes.end(); n++)
+	{
+		double estimate[3];
+		if((*n)->getEstimateData(estimate))
+		{
+			karto::Pose2 pose(estimate[0], estimate[1], estimate[2]);
+			mCorrections.Add(karto::Pair<int, karto::Pose2>((*n)->id(), pose));
+		}else
+		{
+			ROS_ERROR("[g2o] Could not get estimated pose from Optimizer!");
+		}
+	}
 }
 
 void G2oSolver::AddNode(karto::Vertex<karto::LocalizedObjectPtr>* pVertex)
@@ -49,6 +84,7 @@ void G2oSolver::AddNode(karto::Vertex<karto::LocalizedObjectPtr>* pVertex)
 	poseVertex->setEstimate(g2o::SE2(odom.GetX(), odom.GetY(), odom.GetHeading()));
 	poseVertex->setId(pVertex->GetVertexObject()->GetUniqueId());
 	mOptimizer.addVertex(poseVertex);
+	ROS_DEBUG("[g2o] Adding node %d.", pVertex->GetVertexObject()->GetUniqueId());
 }
 
 void G2oSolver::AddConstraint(karto::Edge<karto::LocalizedObjectPtr>* pEdge)
@@ -61,8 +97,18 @@ void G2oSolver::AddConstraint(karto::Edge<karto::LocalizedObjectPtr>* pEdge)
 	int targetID = pEdge->GetTarget()->GetVertexObject()->GetUniqueId();
 	odometry->vertices()[0] = mOptimizer.vertex(sourceID);
 	odometry->vertices()[1] = mOptimizer.vertex(targetID);
-	if(odometry->vertices()[0] == NULL) ROS_ERROR("Source vertex with id %d does not exist!", sourceID);
-	if(odometry->vertices()[0] == NULL) ROS_ERROR("Target vertex with id %d does not exist!", targetID);
+	if(odometry->vertices()[0] == NULL)
+	{
+		ROS_ERROR("[g2o] Source vertex with id %d does not exist!", sourceID);
+		delete odometry;
+		return;
+	}
+	if(odometry->vertices()[0] == NULL)
+	{
+		ROS_ERROR("[g2o] Target vertex with id %d does not exist!", targetID);
+		delete odometry;
+		return;
+	}
 	
 	// Set the measurement (odometry distance between vertices)
 	karto::LinkInfo* pLinkInfo = (karto::LinkInfo*)(pEdge->GetLabel());
@@ -82,6 +128,7 @@ void G2oSolver::AddConstraint(karto::Edge<karto::LocalizedObjectPtr>* pEdge)
 	odometry->setInformation(info);
 	
 	// Add the constraint to the optimizer
+	ROS_DEBUG("[g2o] Adding Edge from node %d to node %d.", sourceID, targetID);
 	mOptimizer.addEdge(odometry);
 }
 
