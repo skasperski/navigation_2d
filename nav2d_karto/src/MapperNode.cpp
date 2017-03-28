c7cc025495187a3f1182d7d4bdbf4637531fd3d8
Sebastian Kasperski <sebastian.kasperski@dfki.de>
2017-03-28 09:28:11 +0200

    Removed g2o switches.

--- nav2d_karto/src/MapperNode.cpp
+++ nav2d_karto/src/MapperNode.cpp
@@ -2,12 +2,7 @@
 
 #include <nav2d_karto/MultiMapper.h>
 #include <nav2d_karto/SpaSolver.h>
-
-#ifdef USE_G2O
-#include <nav2d_karto/G2oSolver.h>
-#else
 #include <nav2d_karto/SpaSolver.h>
-#endif
 
 int main(int argc, char **argv)
 {
@@ -16,11 +11,7 @@ int main(int argc, char **argv)
 	ros::NodeHandle node;
 
 	// Create a scan-solver
-#ifdef USE_G2O
-	G2oSolver* solver = new G2oSolver();
-#else
 	SpaSolver* solver = new SpaSolver();
-#endif
 
 	// Create the MultiMapper
 	MultiMapper mapper;
